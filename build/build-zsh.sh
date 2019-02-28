e733b8b511f8aa1d13d8f8dc02a966085f4c8074
Part Cyborg <me@partcyb.org>
2019-02-28 08:36:49 -0800

    fixed bugs in zshenv fleshed out build script some more

--- build/build-zsh.sh
+++ build/build-zsh.sh
@@ -1,12 +1,21 @@
-#!/bin/false
-# NOTE: this is not meant to be ran, its just documentation for building zsh from source.
-echo "Don't run me!"; exit 1
+#!/bin/zsh
+
+SOURCE_URL="https://github.com/zsh-users/zsh/archive/zsh-5.7.1.tar.gz"
+
+[ -z "$NDK_BUILD" ] && { echo "NDK_BUILD not defined"; exit 1; }
+
+wget "$SOURCE_URL"
+tar xvf zsh-5.7.1.tar.gz
+cd zsh-zsh-5.7.1
+
 
 # First, be sure to copy over the config.modules file from this directory to the root of
 # the source tree.
+cp ../config.modules .
 
 # The following command was used to configure zsh:
 ./configure \
+   --host=aarch64-unknown-linux-gnu\
    --bindir=/system/xbin \
    --sbindir=/system/xbin \
    --libexecdir=/system/xbin \
